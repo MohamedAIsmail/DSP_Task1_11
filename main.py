7aa57d4650eae7ac9b8a8c0c0a6ea0f649119245
MohamedAIsmail <mohamedaismail214@gmail.com>
2022-10-14 18:35:32 +0200

    Main

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-
 import streamlit.components.v1 as components
-
 import time  # to simulate a real time data, time loop
-
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import streamlit as st  # 🎈 data web app development
-
 import matplotlib.pyplot as plt
 import mpld3
 
