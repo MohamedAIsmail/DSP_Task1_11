58bc0ed98ecea161a3476d30f400985b8cda836e
MohamedAIsmail <mohamedaismail214@gmail.com>
2022-10-14 18:32:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+import streamlit.components.v1 as components
+
+import time  # to simulate a real time data, time loop
+
+import numpy as np  # np mean, np random
+import pandas as pd  # read csv, df manipulation
+import plotly.express as px  # interactive charts
+import streamlit as st  # 🎈 data web app development
+
+import matplotlib.pyplot as plt
+import mpld3
+
+# create your figure and get the figure object returned
+fig = plt.figure()
+plt.plot([1, 2, 3, 4, 5])
+
+fig_html = mpld3.fig_to_html(fig)
+components.html(fig_html, height=600)
