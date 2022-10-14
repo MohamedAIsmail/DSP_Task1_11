d14a8f85cd3e9e03225f2e1891659b2ff3d4522a
MohamedAIsmail <mohamedaismail214@gmail.com>
2022-10-14 18:33:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import time  # to simulate a real time data, time loop
 
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
 
 import matplotlib.pyplot as plt
